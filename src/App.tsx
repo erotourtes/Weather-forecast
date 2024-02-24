b968f1e74bb11483aa73b54b6037255ea1ee3129
erotourtes <erotourtes@gmail.com>
2024-02-24 13:24:59 +0200

    Feat: basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <>Hello world!</>;
+  return (
+    <div className="container">
+      <div className="main">
+        <h2>
+          Weather <strong>Forecast</strong>
+        </h2>
+        <input type="text" placeholder="Search your trip" />
+        <div>Trips go here</div>
+        <h4>Week</h4>
+        <div>Week forecast goes here</div>
+      </div>
+      <div className="side"></div>
+    </div>
+  );
 }
 
 export default App;
