788388942d76ac130d7d0f9d2c401c54178e7a42
erotourtes <erotourtes@gmail.com>
2024-02-25 13:48:07 +0200

    Fix: dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
 import styles from "./Dialog.module.css";
 import classNames from "classnames";
 
@@ -7,8 +7,36 @@ interface DialogProps extends PropsWithChildren {
 }
 
 const Dialog = ({ children, isOpen }: DialogProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const handleResize = () => {
+      if (isOpen && ref.current) {
+        const curScroll = window.scrollY;
+        ref.current.style.top = `${curScroll}px`;
+      }
+    };
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      ref.current.style.top = `${window.scrollY}px`;
+      window.addEventListener("resize", handleResize);
+    } else {
+      document.body.style.overflow = "auto";
+      window.removeEventListener("resize", handleResize);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen, ref]);
+
   return (
     <div
+      ref={ref}
       className={classNames(styles.dialog, {
         [styles.open]: isOpen,
         [styles.close]: !isOpen,
