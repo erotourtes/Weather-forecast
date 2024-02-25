3bb9302ec21acd99c38ba1e1d838ee0be8eb5f71
erotourtes <erotourtes@gmail.com>
2024-02-25 12:10:48 +0200

    Refactor: timer math

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -45,11 +45,9 @@ const Timer = ({
   }, [countdown]);
 
   const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+  const hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((countdown / (1000 * 60)) % 60);
+  const seconds = Math.floor((countdown / 1000) % 60);
 
   const times = [
     [days, "Days"],
